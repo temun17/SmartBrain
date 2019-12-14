7a9a486f8a78097f1bf8f5426fa119d612bd6c55
ATemunovic <atemunovic@Allens-MBP.attlocal.net>
2019-12-13 22:03:55 -0800

    added facial bounding-box around to display on picture

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={imageURL} alt="" width="250px" height="auto" />
+        <img
+          id="inputImage"
+          src={imageURL}
+          alt=""
+          width="250px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            left: box.leftCol,
+            right: box.rightCol,
+            bottom: box.bottomRow
+          }}
+        ></div>
       </div>
     </div>
   );
--- src/containers/App.js
+++ src/containers/App.js
@@ -41,20 +41,45 @@ class App extends React.Component {
     super();
 
     this.state = {
-      imageURL: ""
+      imageURL: "",
+      box: {}
     };
   }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputImage");
+
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({
+      box
+    });
+  };
 
   onButtonSubmit = () => {
     this.setState({
       imageURL: this.props.input
     });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.props.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info);
-      },
-      function(err) {}
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.props.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(error => console.log(error));
   };
 
   render() {
@@ -69,7 +94,7 @@ class App extends React.Component {
           onInputChange={onSearchChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageURL={this.state.imageURL} />
+        <FaceRecognition box={this.state.box} imageURL={this.state.imageURL} />
       </div>
     );
   }
