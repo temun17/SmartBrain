67f0c1fd26612a9cb2b82d891dd71b2340916a46
ATemunovic <atemunovic@Allens-MBP.attlocal.net>
2019-12-13 16:54:11 -0800

    front-end is working that is returning real results with Clarifai API face recognition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Clarifai from "clarifai";
 import Nav from "./components/Navigation/Nav";
 import Logo from "./components/Logo/Logo";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "b2760f9e820044308efa89770ba07dab"
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +25,33 @@ const particlesOptions = {
 };
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      imageURL: ""
+    };
+  }
+
+  onInputChange = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({
+      imageURL: this.state.input
+    });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info);
+      },
+      function(err) {}
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -27,8 +59,11 @@ class App extends React.Component {
         <Nav />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL} />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -13,8 +13,12 @@ const ImageLinkForm = () => {
             className="f4 pa2 w-70 center"
             type="text"
             placeholder="Enter url here..."
+            onChange={onInputChange}
           />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <button
+            onClick={onButtonSubmit}
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          >
             Detect
           </button>
         </div>
